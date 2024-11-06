feb66f75e37f123224831dcd93d37df127f7e3b2
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2024-11-06 18:34:56 -0300

    update docs config

--- tests/test_sumeh_dq.py
+++ tests/test_sumeh_dq.py
@@ -12,22 +12,26 @@ class TestQualityFunction(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Load the mock data from the CSV file
-        cls.mock_data_file = os.path.join(os.path.dirname(__file__), 'mock', 'mock_data.csv')
-        cls.mock_config_file = os.path.join(os.path.dirname(__file__), 'mock', 'config_tests.csv')
+        cls.mock_data_file = os.path.join(
+            os.path.dirname(__file__), "mock", "mock_data.csv"
+        )
+        cls.mock_config_file = os.path.join(
+            os.path.dirname(__file__), "mock", "config_tests.csv"
+        )
 
         # Read the mock data into a pandas DataFrame
-        cls.mock_data = pd.read_csv(cls.mock_data_file, sep=';', header=0)
+        cls.mock_data = pd.read_csv(cls.mock_data_file, sep=";", header=0)
 
     def test_quality_with_mock_data(self):
         print(self.mock_data)
         result = quality(
             df=self.mock_data,
-            source_type='csv',
+            source_type="csv",
             file_path=self.mock_config_file,
         )
 
         print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
